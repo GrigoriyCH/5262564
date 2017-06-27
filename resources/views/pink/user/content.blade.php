8387b283476bd1c0735825b91f0bfc3c8aeb46be
GrigoriyCH <grigory.tchuprina@gmail.com>
2017-06-27 17:52:13 +0300

    List user works

--- resources/views/pink/user/content.blade.php
+++ resources/views/pink/user/content.blade.php
@@ -1,23 +1,45 @@
-<div id="content-blog" class="content group">
-	
+<div id="content-page" class="content group">
+	<div class="hentry group">
 		@if($user_post)
 			<div class="short-table white">
 			<table style="width: 100%" cellspacing="0" cellpadding="0">
-			<caption>Все посты пользователя: {{$username}}</caption>
-				<th>Заголовок</th>
-				<th>Текст</th>
-				<th>Дата</th>
-				<th>Действие</th>
+			<tr>
+				<th>Все посты пользователя: {{$username}}</th>
+			</tr>
 			@foreach($user_post as $post)
 				<tr>
-					<td class="align-left"><a href="{{ route('user.userpost.edit',['id'=>$post->id]) }}">{{ $post->title }}</a></td>
-					<td class="align-left">{!!str_limit(strip_tags($post->text, '<a><p><br><strong><i>'),256)!!}</td>
-					<td><p class="date">{{ $post->created_at->format('F d, Y') }}</p></td>
-					<td>
-					{!! Form::open(['url' => route('user.userpost.destroy',['post'=>$post->id]),'class'=>'form-horizontal','method'=>'POST']) !!}
-					{{ method_field('DELETE') }}
-					{!! Form::button('Удалить', ['class' => 'btn btn-french-5','type'=>'submit']) !!}
-					{!! Form::close() !!}
+					<td class="align-left">
+					<div class="recent-post group" style="margin-top:1em;margin-bottom:1em;">
+					<div class="hentry-post group">
+				                        <div class="thumb-img">
+										@if(isset($post->img))
+											<div style="overflow:hidden;width:82px;height:28px;">
+												<div style="width:100%;">
+												{!! Html::image($post->img) !!}
+												</div>
+											</div>
+										@endif
+										</div>
+				                        <div class="text" style="margin-left:100px;">
+											
+											<a href="{{ route('user.userpost.edit',['id'=>$post->id]) }}">{{ $post->title }}</a>
+											
+											@if($post->created_at)
+				                            <p class="post-date">{{ $post->created_at->format('F d, Y') }}</p>
+											@endif
+											
+				                            <p>{!!str_limit(strip_tags($post->text, '<a><p><br><strong><i>'),256)!!}<p>
+											
+											<div style="float:right;">
+												{!! Form::open(['url' => route('user.userpost.destroy',['post'=>$post->id]),'class'=>'form-horizontal','method'=>'POST']) !!}
+												{{ method_field('DELETE') }}
+												{!! Form::button('Удалить', ['class' => 'btn btn-french-5','type'=>'submit']) !!}
+												{!! Form::close() !!}
+											</div>
+											
+				                        </div>
+				    </div>
+					</div>
 					</td>
 				</tr>
 			@endforeach
@@ -26,5 +48,5 @@
 		@else
 		<h3> У вас еще нет постов...<h3>
 		@endif
-				            
+	</div>			            
 </div>
